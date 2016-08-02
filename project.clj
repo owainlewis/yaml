845b43aef7d7a3b1d0d3918885631f23554c9490
matthew_gilliard <matthew.gilliard@gmail.com>
2016-08-02 11:27:43 +0100

    Adds support for setting the scalar-style

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.forward/yaml "1.0.3"
+(defproject io.forward/yaml "1.0.4-SNAPSHOT"
   :description "A YAML library for Clojure"
   :url "http://github.com/owainlewis/yaml"
   :license {:name "Eclipse Public License"
--- test/yaml/writer_test.clj
+++ test/yaml/writer_test.clj
@@ -4,7 +4,19 @@
 
 (deftest dump-opts
   (let [data [{:age 33 :name "jon"} {:age 44 :name "boo"}]]
+
     (is (= "- age: 33\n  name: jon\n- age: 44\n  name: boo\n"
            (generate-string data :dumper-options {:flow-style :block})))
     (is (= "[{age: 33, name: jon}, {age: 44, name: boo}]\n"
-           (generate-string data :dumper-options {:flow-style :flow})))))
+           (generate-string data :dumper-options {:flow-style :flow})))
+
+    (is (= "- \"age\": !!int \"33\"\n  \"name\": \"jon\"\n- \"age\": !!int \"44\"\n  \"name\": \"boo\"\n"
+           (generate-string data :dumper-options {:scalar-style :double-quoted})))
+    (is (= "- 'age': !!int '33'\n  'name': 'jon'\n- 'age': !!int '44'\n  'name': 'boo'\n"
+           (generate-string data :dumper-options {:scalar-style :single-quoted})))
+    (is (= "- \"age\": !!int |-\n    33\n  \"name\": |-\n    jon\n- \"age\": !!int |-\n    44\n  \"name\": |-\n    boo\n"
+           (generate-string data :dumper-options {:scalar-style :literal})))
+    (is (= "- \"age\": !!int >-\n    33\n  \"name\": >-\n    jon\n- \"age\": !!int >-\n    44\n  \"name\": >-\n    boo\n"
+           (generate-string data :dumper-options {:scalar-style :folded})))
+    (is (= "- {age: 33, name: jon}\n- {age: 44, name: boo}\n"
+           (generate-string data :dumper-options {:scalar-style :plain})))))
