5f28ea6ca50dbe39558c3e32b1fb00d6e467c46f
Owain Lewis <owain@owainlewis.com>
2017-03-07 20:04:46 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.forward/yaml "1.0.5"
+(defproject io.forward/yaml "1.0.6"
   :description "A YAML library for Clojure"
   :url "http://github.com/owainlewis/yaml"
   :license {:name "Eclipse Public License"
