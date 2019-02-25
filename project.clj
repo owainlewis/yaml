6ccc1a53527d537113f217e1cbd19fa48a82134e
Alan Thompson <cloojure@gmail.com>
2019-02-25 15:15:48 -0800

    misc

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.flatland/ordered "1.5.7"]
                  [org.yaml/snakeyaml "1.23"]
-                 [org.flatland/ordered "1.5.7"]]
+                ;[org.snakeyaml/snakeyaml-engine "1.0"]
+                ]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
                                     :password :env/clojars_password
