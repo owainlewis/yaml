f2aecd6c34f32d757f03354ed065c9888331ab0e
Crispin Wellington <retrogradeorbit@gmail.com>
2019-10-13 21:06:08 +0800

    bump snakeyaml dependency to newest stable version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.yaml/snakeyaml "1.19"]
+                 [org.yaml/snakeyaml "1.25"]
                  [org.flatland/ordered "1.5.7"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
