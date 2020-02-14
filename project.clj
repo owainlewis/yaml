211303b4844e11dcfbea6ed1c9814dc907d56991
Owain Lewis <owain.lewis@oracle.com>
2020-02-14 11:46:04 +0000

    Bump project version for new release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.forward/yaml "1.0.9"
+(defproject io.forward/yaml "1.0.10"
   :description "A YAML library for Clojure"
   :url "http://github.com/owainlewis/yaml"
   :license {:name "Eclipse Public License"
